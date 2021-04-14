b350319beed40b82b5d4d82bd72b505ec09fe9b7
Nicolas Chamorro <nichagiro@gmail.com>
2021-04-14 17:48:24 -0500

    primer commit, plantilla funcional, editar estrcutra codigo

---
+++ plantilla/js/foot.js
@@ -0,0 +1,56 @@
+var elementorFrontendConfig = {
+    "environmentMode": {
+        "edit": false,
+        "wpPreview": false,
+        "isScriptDebug": false,
+        "isImprovedAssetsLoading": false
+    },
+    "i18n": {
+        "shareOnFacebook": "Share on Facebook",
+        "shareOnTwitter": "Share on Twitter",
+        "pinIt": "Pin it",
+        "download": "Download",
+        "downloadImage": "Download image",
+        "fullscreen": "Fullscreen",
+        "zoom": "Zoom",
+        "share": "Share",
+        "playVideo": "Play Video",
+        "previous": "Previous",
+        "next": "Next",
+        "close": "Close"
+    },
+    "is_rtl": false,
+    "breakpoints": {
+        "xs": 0,
+        "sm": 480,
+        "md": 768,
+        "lg": 1025,
+        "xl": 1440,
+        "xxl": 1600
+    },
+    "version": "3.1.4",
+    "is_static": false,
+    "experimentalFeatures": [],
+    "urls": {
+        "assets": "https:\/\/demo.athemes.com\/sydney\/wp-content\/plugins\/elementor\/assets\/"
+    },
+    "settings": {
+        "page": [],
+        "editorPreferences": []
+    },
+    "kit": {
+        "global_image_lightbox": "yes",
+        "lightbox_enable_counter": "yes",
+        "lightbox_enable_fullscreen": "yes",
+        "lightbox_enable_zoom": "yes",
+        "lightbox_enable_share": "yes",
+        "lightbox_title_src": "title",
+        "lightbox_description_src": "description"
+    },
+    "post": {
+        "id": 13,
+        "title": "Sydney",
+        "excerpt": "",
+        "featuredImage": false
+    }
+};
