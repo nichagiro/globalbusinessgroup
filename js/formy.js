f42c548e9b1a32833bc2383c94fa96121dce567f
Nicolas Chamorro <nichagiro@gmail.com>
2021-04-18 20:12:10 -0500

    arreglos diseño, form contact full, seo basic

--- js/formy.js
+++ js/formy.js
@@ -1,22 +1,42 @@
-var formy = document.getElementById('formy');
+const formy = document.getElementById('formy');
+var envio = document.getElementById('envio');
 
-formy.addEventListener('click',function (e) {
+
+envio.addEventListener('click',function (e) {
     
     e.preventDefault();
 
-    const form = FormData(formy);
+    const form = new FormData(formy);
 
-    fetch('php/formi.php'{
+    fetch('php/formi.php',{
         body:form,
         method:'post'
     })
     .then(res => res.json())
     .then(data => {
+
         if (data) {
-            
+            envio_alert('success', true);
         }
-    })
+        else{
+            envio_alert('danger', false);
+        }
+    });
 
 })
 
+function envio_alert (alert, correo) {
+    let alerta = document.getElementById(alert);
+    alerta.style.display = 'block';
+
+    if (correo) {
+        formy.reset();
+        envio.disabled = true;
+        envio.innerHTML = 'Gracias'
+    }
+
+    setTimeout(() => {
+        alerta.style.display = 'none';
+    }, 3500);
+}
 
--- php/formi.php
+++ php/formi.php
@@ -1,2 +1,29 @@
 <?php
 
+$name = $_POST['name'];
+$asunto = $_POST['asunto'];
+$phone = $_POST['phone'];
+$msg = $_POST['msg'];
+$email = $_POST['email'];
+
+if($email == '' | $name == '' | $msg =='' | $phone =='' | $asunto ==''){
+ 
+    echo json_encode(false);
+ 
+}   else {
+ 
+// $to = 'comercial@globalbusinessgroup.co';
+// $subject = "Correo desde pagina web";
+// $message =  "Email: $email".
+//             "\nNombre: $name".
+//             "\nTelefono: $phone".
+//             "\nAsunto: $asunto".
+//             "\nMensaje: $msg";
+ 
+// $headers = "From:".$name.' <'.$email.'>';
+ 
+// mail($to, $subject, $message, $headers);
+ 
+echo json_encode (true);
+ 
+}
